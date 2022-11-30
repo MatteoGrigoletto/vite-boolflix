0b28e09bd52bca249bd7e05e6f4eecb3408a9926
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-11-30 12:29:18 +0100

    Aggiunge input con v-model per la ricarca filtrata dei film tramite api

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,26 @@
 </template>
 
 <script>
+import {store} from './store';
+
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
   export default {
     data(){
       return{
-
+        store,
       }
+    },
+    methods:{
+
     },
     components:{
       AppHeader,
       AppMain,
       AppFooter,
 
-    }
+    },
   }
 </script>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,37 @@
 <template>
     <div>
-ciao
+        <form action="" @submit.prevent="filtering">
+            <input type="text" v-model="store.title">
+            <button > Cerca</button>
+        </form>
     </div>
 </template>
 
 <script>
+import {store} from '../store';
+import axios from 'axios';
     export default {
         name:'AppHeader',
+        data(){
+            return{
+                store,
+            }
+        },
+        methods: {
+// aggiunge metodo per la ricerca filtrata dei film
+            filtering(){
+                axios.get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key: '8e140fa3ab237a4b70f6371d23a8cd27',
+          query: this.store.title,
+        }
+      })
+      .then((response) =>{
+        this.store.listFilm = response.data.results;
+        console.log(this.store.listFilm)
+      })
+            }
+        },
     }
 </script>
 
