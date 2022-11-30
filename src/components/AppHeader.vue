3e87e699b51690ece71f1455bf429c5c4de327a8
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-11-30 14:23:08 +0100

    Aggiunge immagini senza controllo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -28,9 +28,18 @@ import axios from 'axios';
       })
       .then((response) =>{
         this.store.listFilm = response.data.results;
-        console.log(this.store.listFilm)
+      }),
+      axios.get('https://api.themoviedb.org/3/search/tv', {
+        params: {
+          api_key: '8e140fa3ab237a4b70f6371d23a8cd27',
+          query: this.store.title,
+        }
       })
-            }
+      .then((response) =>{
+        this.store.listTv = response.data.results
+        console.log(this.store.listTv)
+      })
+            },   
         },
     }
 </script>
