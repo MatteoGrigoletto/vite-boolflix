622e64045784a2cde4f92c94d04c4a277bf4c9fc
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-11-30 17:29:02 +0100

    Aggiunge stelle e flag tramite ciclo for all'interno del font e condizione if per le flag

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -3,15 +3,31 @@
         <img :src="`http://image.tmdb.org/t/p/w500/${film.poster_path}`" alt="">
         <h3>{{film.title}}</h3>
         <p>{{film.original_title}}</p>
-        <p>{{film.original_language}}</p>
-        <p>{{film.vote_average}}</p>
+        <div v-if="film.original_language === 'it'">
+            <img src="../assets/italy.png" alt="">
+        </div>
+        <div v-else-if="film.original_language === 'en'">
+            <img src="../assets/unking.png" alt="">
+        </div>
+        <div>
+            <i class="fa-solid fa-star" v-for=" index in 5"
+            :class="startsCalcolate(film.vote_average)  > index ? 'gold' : 'gray'" ></i>
+        </div>
     </div>
     <div v-for="tv in store.listTv">
         <img :src="`http://image.tmdb.org/t/p/w500/${tv.poster_path}`" alt="">
         <h3>{{tv.name}}</h3>
         <p>{{tv.original_name}}</p>
-        <p>{{tv.original_language}}</p>
-        <p>{{tv.vote_average}}</p>
+        <div v-if="tv.original_language === 'it'">
+            <img src="../assets/italy.png" alt="">
+        </div>
+        <div v-else-if="tv.original_language === 'en'">
+            <img src="../assets/unking.png" alt="">
+        </div>
+        <div>
+            <i class="fa-solid fa-star" v-for=" index in 5"
+            :class="startsCalcolate(tv.vote_average)  > index ? 'gold' : 'gray'" ></i>
+        </div>
     </div>
 </template>
 
@@ -23,10 +39,20 @@ import {store} from '../store';
             return{
                 store,
             }
+        },
+        methods: {
+            startsCalcolate(vote){
+               return Math.floor(vote / 2);
+            }
         }
     }
 </script>
 
 <style lang="scss" scoped>
-
+ .gray {
+    color: #ededed;
+}
+.gold {
+    color: yellow;
+}
 </style>
