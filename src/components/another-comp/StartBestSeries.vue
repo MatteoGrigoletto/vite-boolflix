1cc4dbf0ccc204dce475a622cde02e4e745625d0
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-12-02 11:11:35 +0100

    Fixato error slider

--- src/components/another-comp/StartBestSeries.vue
+++ src/components/another-comp/StartBestSeries.vue
@@ -5,21 +5,21 @@
         <section id="section4">
             <a href="#section6" class="arrow__btn">‹</a>
             <div class="item" v-for="index in 5">
-                <img  :src="`http://image.tmdb.org/t/p/w500/${seriesList[index].poster_path}`" alt="">
+                <img  :src="`http://image.tmdb.org/t/p/w500/${seriesList[index]?.poster_path}`" alt="">
         </div>
             <a href="#section5" class="arrow__btn">›</a>
         </section>
         <section id="section5">
             <a href="#section1" class="arrow__btn">‹</a>
             <div class="item" v-for="index in 5">
-                <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[index + 5].poster_path}`" alt="">
+                <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[index + 5]?.poster_path}`" alt="">
         </div>
             <a href="#section6" class="arrow__btn">›</a>
         </section>
         <section id="section6">
             <a href="#section4" class="arrow__btn">‹</a>
             <div class="item" v-for="index in 5">
-                <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[index + 10].poster_path}`" alt="">
+                <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[index + 10]?.poster_path}`" alt="">
         </div>
             <a href="#section4" class="arrow__btn">›</a>
         </section>
